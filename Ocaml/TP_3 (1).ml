0807cc15098f36455a6bf169b0c827f0561e850d
Chouettemortua <chouettemortua@gmail.com>
2024-09-15 10:53:18 +0200

    gitignore

--- Ocaml/TP_3 (1).ml
+++ Ocaml/TP_3 (1).ml
@@ -9,13 +9,11 @@ let annee: entier_etendu = Entier 2024;;
 let pos_inf: entier_etendu = Plus_Inf;;
 let neg_inf: entier_etendu = Moins_Inf;;
 
-let ajoute_etendu x y =
+let rec ajoute_etendu x y =
   match x,y with
-  | Moins_Inf, Plus_Inf -> failwith("cas indeter")
-  | Plus_Inf, Moins_Inf -> failwith("cas indeter")
+  | Moins_Inf, Plus_Inf -> failwith("cas indetermine +oo -oo")
   | Moins_Inf, _ -> Moins_Inf
   | Plus_Inf, _ -> Plus_Inf
-  | _, Moins_Inf -> Moins_Inf
-  | _, Plus_Inf -> Plus_Inf
   |Entier a,Entier b -> Entier (a+b)
+  | _,_ -> ajoute_etendu y x
 ;;
